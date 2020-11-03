47d89e158c2874f1bb89c2133afbb5c4e3baa1ce
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-11-04 06:40:51 +0700

    #5 Request Data

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    //di sini isi controller pegawai
+    
+    public function index($nama){
+ 
+    	return $nama;
+ 
+    }
+
+    public function formulir(){
+ 
+    	return view('formulir');
+ 
+    }
+
+    public function proses(Request $request){
+        $nama = $request->input('nama');
+     	$alamat = $request->input('alamat');
+        return "Nama : ".$nama.", Alamat : ".$alamat;
+    }       
 }
