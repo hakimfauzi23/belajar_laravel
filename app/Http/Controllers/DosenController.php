7608609919a87d07abadbcca052118adeefcc0ab
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-11-04 06:27:33 +0700

    #4 Passing Data Ke View

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class DosenController extends Controller
 {
     public function index(){
-        return "Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com";
+        $nama = "Diki Alfarabi Hadi";
+    	$pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+ 
+    	return view('biodata',['nama' => $nama , 'matkul' => $pelajaran]);
     }
 }
