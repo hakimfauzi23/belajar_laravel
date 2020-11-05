dfbb1fb1aceedfeaf041eb495cbc0b465e5692eb
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-11-05 12:57:30 +0700

    #10 CRUD : Input data to database

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -23,15 +23,38 @@ class PegawaiController extends Controller
  
     }
 
-    public function formulir(){
- 
-    	return view('formulir');
- 
+    // method untuk menampilkan view form tambah pegawai
+    public function tambah()
+    {
+
+	// memanggil view tambah
+	return view('tambah');
+
     }
 
-    public function proses(Request $request){
-        $nama = $request->input('nama');
-     	$alamat = $request->input('alamat');
-        return "Nama : ".$nama.", Alamat : ".$alamat;
-    }       
+    // method untuk insert data ke table pegawai
+    public function store(Request $request)
+{
+	// insert data ke table pegawai
+	DB::table('pegawai')->insert([
+		'pegawai_nama' => $request->nama,
+		'pegawai_jabatan' => $request->jabatan,
+		'pegawai_umur' => $request->umur,
+		'pegawai_alamat' => $request->alamat
+	]);
+	// alihkan halaman ke halaman pegawai
+	return redirect('/pegawai');
+    }
+
+    // public function formulir(){
+ 
+    // 	return view('formulir');
+ 
+    // }
+
+    // public function proses(Request $request){
+    //     $nama = $request->input('nama');
+    //  	$alamat = $request->input('alamat');
+    //     return "Nama : ".$nama.", Alamat : ".$alamat;
+    // }       
 }
