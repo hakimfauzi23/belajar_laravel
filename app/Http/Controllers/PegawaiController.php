59ad300ed484a6e6573e9d078e577db24fe606a1
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-11-05 13:57:15 +0700

    #11 CRUD : Update Data

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -34,7 +34,7 @@ class PegawaiController extends Controller
 
     // method untuk insert data ke table pegawai
     public function store(Request $request)
-{
+    {
 	// insert data ke table pegawai
 	DB::table('pegawai')->insert([
 		'pegawai_nama' => $request->nama,
@@ -44,6 +44,29 @@ class PegawaiController extends Controller
 	]);
 	// alihkan halaman ke halaman pegawai
 	return redirect('/pegawai');
+    }
+
+    // method untuk edit data pegawai
+    public function edit($id)
+    {
+        // mengambil data pegawai berdasarkan id yang dipilih
+        $pegawai = DB::table('pegawai')->where('pegawai_id',$id)->get();
+        // passing data pegawai yang didapat ke view edit.blade.php
+        return view('edit',['pegawai' => $pegawai]);
+    }
+
+    // update data pegawai
+    public function update(Request $request)
+    {
+        // update data pegawai
+        DB::table('pegawai')->where('pegawai_id',$request->id)->update([
+            'pegawai_nama' => $request->nama,
+            'pegawai_jabatan' => $request->jabatan,
+            'pegawai_umur' => $request->umur,
+            'pegawai_alamat' => $request->alamat
+        ]);
+        // alihkan halaman ke halaman pegawai
+        return redirect('/pegawai');
     }
 
     // public function formulir(){
